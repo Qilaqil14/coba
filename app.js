1f8e7a4eaa836670539171d61bdcbaf920d903bc
Mukhamad Sa'id Aqil <142391294+Qilaqil14@users.noreply.github.com>
2023-10-24 22:39:58 +0700

    h

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Hello Programmer Gaul!')
 })
-require("dotenv").config()
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server is running in port ${port}...`));
